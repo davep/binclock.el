8122720c9586935238aa0ecc871719cfe0195477
Dave Pearson <davep@davep.org>
2017-03-25 18:11:28 +0000

    v1.8

--- binclock.el
+++ binclock.el
@@ -2,7 +2,7 @@
 ;; Copyright 1999-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.7
+;; Version: 1.8
 ;; Keywords: time display
 ;; URL: https://github.com/davep/binclock.el
 ;; Package-Requires: ((cl-lib "0.5"))
