2e529ace67a04e6872a2328769782ef33b0e463a
Dave Pearson <davep@davep.org>
2017-04-18 16:12:44 +0100

    v1.10

--- binclock.el
+++ binclock.el
@@ -2,7 +2,7 @@
 ;; Copyright 1999-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.9
+;; Version: 1.10
 ;; Keywords: games, time, display
 ;; URL: https://github.com/davep/binclock.el
 ;; Package-Requires: ((cl-lib "0.5"))
